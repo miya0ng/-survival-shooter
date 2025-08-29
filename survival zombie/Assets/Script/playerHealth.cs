b089653c5f4c26a87323e9684684b5d708df9e55
이승연 <leelaf0002@gmail.com>
2025-08-29 18:00:46 +0900

    playerHealth 구현중

---
+++ survival zombie/Assets/Script/playerHealth.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerHealth : LivingEntity
+{
+    //private override void Die()
+    //{
+    //    base.Die();
+    //    OnDeath += () => ;
+    //}
+
+    //private override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+    //{
+    //    base.OnDamage(damage, hitPoint, hitNormal);
+    //}
+}
